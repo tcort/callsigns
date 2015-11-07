f441a68701c6a49292d34fbd4da27369cf425c29
Thomas Cort <linuxgeek@gmail.com>
2015-11-07 08:28:06 -0500

    index: use pool.query()
    node-mysql provides an interface for running a single query
    instead of doing pool.getConnection, conn.query(), conn.release().
    Use that interface as it's simpler and less code.

--- index.js
+++ index.js
@@ -87,49 +87,29 @@ app.get('/', function getRoot(req, res) {
     var limit = 10;
     var offset = limit * (page - 1);
 
-    pool.getConnection(function getConnection(err, conn) {
-        if (err) {
+    pool.query('SELECT SQL_CALC_FOUND_ROWS *, MATCH(`callsign`,`first_name`,`surname`,`address_line`,`city`,`prov_cd`,`postal_code`,`club_name`,`club_name_2`,`club_address`,`club_city`,`club_prov_cd`,`club_postal_code`) AGAINST (? IN NATURAL LANGUAGE MODE) AS relevance FROM callsigns WHERE MATCH(`callsign`,`first_name`,`surname`,`address_line`,`city`,`prov_cd`,`postal_code`,`club_name`,`club_name_2`,`club_address`,`club_city`,`club_prov_cd`,`club_postal_code`) AGAINST (? IN NATURAL LANGUAGE MODE) HAVING relevance > 0.2 ORDER BY relevance DESC LIMIT ?, ?; SELECT FOUND_ROWS() AS num_results', [ req.query.q, req.query.q, offset, limit ], function query(err, results) {
+        if (err) { // error, display error
             res.status(500).render('index', { err: { code: 500, type: 'danger', message: res.__("Database error. Please try again later.") }, result: null, resultSet: null });
-            if (conn) {
-                conn.release();
-            }
-            return;
+        } else if (results[0].length === 0) { // no results, display no results message
+            res.status(404).render('index', { err: { code: 404, type: 'info', message: res.__("No results found.") }, result: null, resultSet: null });
+        } else if (results[0].length === 1 && !req.query.page) { // one result, display it
+            res.redirect('/callsigns/' + results[0][0].callsign);
+        } else { // many results, display a paginated list
+            res.render('index', { err: null, result: null, resultSet: results[0], pagination: { page: page, pageCount: Math.ceil(results[1][0].num_results / limit) }, num_results: results[1][0].num_results, q: req.query.q });
         }
-        conn.query('SELECT SQL_CALC_FOUND_ROWS *, MATCH(`callsign`,`first_name`,`surname`,`address_line`,`city`,`prov_cd`,`postal_code`,`club_name`,`club_name_2`,`club_address`,`club_city`,`club_prov_cd`,`club_postal_code`) AGAINST (? IN NATURAL LANGUAGE MODE) AS relevance FROM callsigns WHERE MATCH(`callsign`,`first_name`,`surname`,`address_line`,`city`,`prov_cd`,`postal_code`,`club_name`,`club_name_2`,`club_address`,`club_city`,`club_prov_cd`,`club_postal_code`) AGAINST (? IN NATURAL LANGUAGE MODE) HAVING relevance > 0.2 ORDER BY relevance DESC LIMIT ?, ?; SELECT FOUND_ROWS() AS num_results', [ req.query.q, req.query.q, offset, limit ], function query(err, results) {
-            if (err) { // error, display error
-                res.status(500).render('index', { err: { code: 500, type: 'danger', message: res.__("Database error. Please try again later.") }, result: null, resultSet: null });
-            } else if (results[0].length === 0) { // no results, display no results message
-                res.status(404).render('index', { err: { code: 404, type: 'info', message: res.__("No results found.") }, result: null, resultSet: null });
-            } else if (results[0].length === 1 && !req.query.page) { // one result, display it
-                res.redirect('/callsigns/' + results[0][0].callsign);
-            } else { // many results, display a paginated list
-                res.render('index', { err: null, result: null, resultSet: results[0], pagination: { page: page, pageCount: Math.ceil(results[1][0].num_results / limit) }, num_results: results[1][0].num_results, q: req.query.q });
-            }
-            conn.release();
-        });
     });
 });
 
 // Display a single result
 app.get('/callsigns/:callsign', function getCallsign(req, res) {
-    pool.getConnection(function getConnect(err, conn) {
-        if (err) {
+    pool.query('SELECT * FROM callsigns WHERE callsign = ?', [ req.params.callsign ], function query(err, results) {
+        if (err || results.length > 1) { // error, display error
             res.status(500).render('index', { err: { code: 500, type: 'danger', message: res.__("Database error. Please try again later.") }, result: null, resultSet: null });
-            if (conn) {
-                conn.release();
-            }
-            return;
+        } else if (results.length === 0) { // no results, display no results message
+            res.status(404).render('index', { err: { code: 404, type: 'info', message: res.__("No results found.") }, result: null, resultSet: null });
+        } else if (results.length === 1) { // one result, display it
+            res.render('index', { err: null, result: results[0], resultSet: null });
         }
-        conn.query('SELECT * FROM callsigns WHERE callsign = ?', [ req.params.callsign ], function query(err, results) {
-            if (err || results.length > 1) { // error, display error
-                res.status(500).render('index', { err: { code: 500, type: 'danger', message: res.__("Database error. Please try again later.") }, result: null, resultSet: null });
-            } else if (results.length === 0) { // no results, display no results message
-                res.status(404).render('index', { err: { code: 404, type: 'info', message: res.__("No results found.") }, result: null, resultSet: null });
-            } else if (results.length === 1) { // one result, display it
-                res.render('index', { err: null, result: results[0], resultSet: null });
-            }
-            conn.release();
-        });
     });    
 });
 
